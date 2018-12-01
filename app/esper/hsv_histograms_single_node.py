83b3f54316b2a1b7eb8d8789092d6750a5489fd5
Dan Fu <danfu09@gmail.com>
2018-12-01 01:38:59 +0000

    HSV histogram script

---
+++ app/esper/hsv_histograms_single_node.py
@@ -0,0 +1,26 @@
+import scannerpy 
+import scannertools as st
+import os
+from django.db.models import Q
+from query.models import Video, VideoTag
+from esper.prelude import Notifier
+
+video_ids = [vals[0] for vals in
+        VideoTag.objects.values_list('video_id').distinct().all()]
+videos = Video.objects.filter(id__in=video_ids)
+
+db = scannerpy.Database()
+hsv_histograms = st.histograms.compute_hsv_histograms(
+    db,
+    videos=[video.for_scannertools() for video in videos]
+)
+
+# Labeler for this pipeline
+LABELER, _ = Labeler.objects.get_or_create(name='hsvhists')
+LABELED_TAG, _ = Tag.objects.get_or_create(name='hsvhists:labeled')
+
+# Tag this video as being labeled
+new_videotags = [VideoTag(video=video, tag=LABELED_TAG) for video in videos]
+VideoTag.objects.bulk_create(new_videotags)
+
+Notifier().notify('Done with HSV histograms')

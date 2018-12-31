7d94825a215dcea972fae8e4aba6b6ca8ea6df75
Dan Fu <danfu09@gmail.com>
2018-12-31 06:10:17 +0000

    Brightness computation and save to database

--- app/esper/brightness_compute.py
+++ app/esper/brightness_compute.py
@@ -40,57 +40,56 @@ frames = [
 # Cluster parameters
 cfg = cluster_config(num_workers=80, worker=worker_config('n1-standard-32'),
     pipelines=[st.gender_detection.GenderDetectionPipeline])
-with make_cluster(cfg, no_delete=True) as db_wrapper:
-    db = db_wrapper.db
-#if True:
-#    db = scannerpy.Database() 
+#with make_cluster(cfg) as db_wrapper:
+#    db = db_wrapper.db
+if True:
+    db = scannerpy.Database() 
 
     print("Detecting brightness")
     # Calculate brightness
     brightness = st.imgproc.compute_brightness_cpp(
         db,
         videos=[video.for_scannertools() for video in videos],
-        frames=frames,
-        cache=False
+        frames=frames
     )
 
-    #print("Saving brightness values in database")
-    ## Figure out which frames we've already computed face embeddings for
-    #frames_in_db_already = set([
-    #    (f.video_id, f.number)
-    #    for f in Frame.objects.filter(tags=LABELED_TAG).all()
-    #])
+    print("Saving brightness values in database")
+    # Figure out which frames we've already computed face embeddings for
+    frames_in_db_already = set([
+        (f.video_id, f.number)
+        for f in Frame.objects.filter(tags=LABELED_TAG).all()
+    ])
 
-    ## Tag all the frames as being labeled, and update frames with brightness val
-    #for video, framelist, brightness_list in zip(videos, frames, brightness):
-    #    new_frame_tags = []
-    #    frames_to_update = []
-    #    frame_objs = Frame.objects.filter(video_id=video.id,
-    #            number__in=framelist).order_by('number')
-    #    for (frame_obj, brightness_val) in zip(frame_objs, brightness_list.load()):
-    #        if (video.id, frame_obj.number) in frames_in_db_already:
-    #            continue
-    #        new_frame_tags.append(
-    #                Frame.tags.through(frame_id=frame_obj.pk, tag_id=LABELED_TAG.pk))
-    #        frame_obj.brightness = brightness_val
-    #        frames_to_update.append(frame_obj)
-    #    with transaction.atomic():
-    #        for frame in frames_to_update:
-    #            frame.save()
-    #        Frame.tags.through.objects.bulk_create(new_frame_tags)
+    # Tag all the frames as being labeled, and update frames with brightness val
+    for video, framelist, brightness_list in tqdm(zip(videos, frames, brightness), total=len(videos)):
+        new_frame_tags = []
+        frames_to_update = []
+        frame_objs = Frame.objects.filter(video_id=video.id,
+                number__in=framelist).order_by('number')
+        for (frame_obj, brightness_val) in zip(frame_objs, brightness_list.load()):
+            if (video.id, frame_obj.number) in frames_in_db_already:
+                continue
+            new_frame_tags.append(
+                    Frame.tags.through(frame_id=frame_obj.pk, tag_id=LABELED_TAG.pk))
+            frame_obj.brightness = brightness_val
+            frames_to_update.append(frame_obj)
+        with transaction.atomic():
+            for frame in frames_to_update:
+                frame.save()
+            Frame.tags.through.objects.bulk_create(new_frame_tags)
 
-    ## Get the videos that already have the tag
-    #videos_tagged_already = set([
-    #    vtag.video_id
-    #    for vtag in VideoTag.objects.filter(tag=LABELED_TAG).all()
-    #])
+    # Get the videos that already have the tag
+    videos_tagged_already = set([
+        vtag.video_id
+        for vtag in VideoTag.objects.filter(tag=LABELED_TAG).all()
+    ])
 
-    ## Tag this video as being labeled
-    #new_videotags = [
-    #    VideoTag(video=video, tag=LABELED_TAG)
-    #    for video in videos
-    #    if video.id not in videos_tagged_already
-    #]
-    #VideoTag.objects.bulk_create(new_videotags)
+    # Tag this video as being labeled
+    new_videotags = [
+        VideoTag(video=video, tag=LABELED_TAG)
+        for video in videos
+        if video.id not in videos_tagged_already
+    ]
+    VideoTag.objects.bulk_create(new_videotags)
 
 Notifier().notify('Done with brightness computation')

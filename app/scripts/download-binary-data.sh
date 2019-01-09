b58dd662d52258000e017ff5d86a90f37c65e12d
Dan Fu <danfu09@gmail.com>
2019-01-09 09:20:17 +0000

    Update script to download caption index

--- app/scripts/download-binary-data.sh
+++ app/scripts/download-binary-data.sh
@@ -11,13 +11,16 @@ cd $DATA
 
 # Download face embeddings
 gsutil -m cp gs://esper/movie-metadata/movie-embeddings.tar.gz .
-tar xvzf movie-embeddings.tar.gz
+tar -xvzf movie-embeddings.tar.gz
 
 # Download face landmarks
 gsutil -m cp gs://esper/movie-metadata/movie-face-landmarks.tar.gz .
-tar xvzf movie-face-landmarks.tar.gz
+tar -xvzf movie-face-landmarks.tar.gz
 
 # Download poses
 gsutil -m cp gs://esper/movie-metadata/movie-poses.tar.gz .
-tar xvzf movie-poses.tar.gz
+tar -xvzf movie-poses.tar.gz
 
+# Download caption index
+gsutil -m cp gs://esper/movie-metadata/aligned-captions.tar.gz .
+tar -xvzf aligned-captions.tar.gz
